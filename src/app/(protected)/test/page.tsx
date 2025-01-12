201e1e51ecc66c10e4a7fc60a264d8f601428030
Bạch Gia Huy <bghuy2003@gmail.com>
2025-01-12 17:39:26 +0700

    test cookie 10

--- src/app/(protected)/test/page.tsx
+++ src/app/(protected)/test/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button"
 import axiosInstance from "@/setup/axios";
+import { decodeJwt } from "jose";
 export default function TestPage () {
     const getCookie = async () => {
         const response = await fetch('https://chat-asp-api.vercel.app/auth/test-cookie', {
@@ -17,6 +18,21 @@ export default function TestPage () {
 
         const response2 = await axiosInstance.get('/auth/test-cookie');
         console.log(response2.data, "data2");
+        
+        const baseURL = process.env.NEXT_PUBLIC_SERVER_MODE === 'production' ? process.env.NEXT_PUBLIC_SERVER_PRODUCTION_URL : process.env.NEXT_PUBLIC_SERVER_DEVELOPMENT_URL;
+        const response3 = await fetch(`${baseURL}/auth/refresh-token`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Control-Allow-Credentials": "true",
+            },
+            credentials: 'include',
+        });
+        const data3 = await response3.json();
+        console.log(data3.data.access_token,"data");
+        const newAccessToken = data3?.data?.access_token;
+        const decodedToken = decodeJwt(newAccessToken);
+        console.log(decodedToken,"decodedToken");
     }
     
     return(
